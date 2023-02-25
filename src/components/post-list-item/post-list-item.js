f8e7dbf29f08cbb84184eed8be3a91b23962cd30
ekkaira <denysxchebotar@gmail.com>
2023-02-25 13:57:04 +0200

    setState practice and custom states with immutable state

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -26,7 +26,7 @@ export default class PostListItem extends Component {
   }
 
   render() {
-    const { label } = this.props;
+    const { label, onDelete } = this.props;
     const { important, like } = this.state;
 
     let classNames = "app-list-item d-flex justify-content-between";
@@ -52,7 +52,7 @@ export default class PostListItem extends Component {
           >
             <i className="bi bi-star-fill"></i>
           </button>
-          <button type="button" className="btn-trash btn-sm">
+          <button type="button" className="btn-trash btn-sm" onClick={onDelete}>
             <i className="bi bi-trash-fill"></i>
           </button>
           <i className="bi bi-heart-fill"></i>
